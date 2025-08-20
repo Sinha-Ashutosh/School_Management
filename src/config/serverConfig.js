5fee115bf52e427ca30a635c5c6ee65b8630cd41
Ashutosh Sinha <Ashutosh-Sinha@outlook.com>
2025-08-20 18:31:30 +0530

    CHanges

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -3,5 +3,20 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 module.exports = {
-    PORT: process.env.PORT
+    PORT: process.env.PORT,
+
+    development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "mysql"
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "mysql"
+    }
 }
